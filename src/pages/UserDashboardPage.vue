12fb28cf46cfd4eddeb7d2f448376a3239af5e4b
steven <steventevoortwis@gmail.com>
2024-03-24 18:00:04 +0100

    Added show of your own recipes in the form of cards in the home of your account

--- src/pages/UserDashboardPage.vue
+++ src/pages/UserDashboardPage.vue
@@ -1,13 +1,53 @@
 <template>
-  claskjd
-    <h1>User dashboard page</h1>
+  <h1>User dashboard page</h1>
+  <div v-for="recipe in userRecipes" style="cursor: pointer" :key="recipe.id">
+    <q-card style="margin-top: 15px" @click="$router.push(`user/e/${recipe.id}`)">
+      {{ recipe}}
+    </q-card>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
+import {getUserRecipes} from "src/firebase/firestore/firestore-recipes";
+import firebase from "firebase/compat";
 
 export default defineComponent({
   name: 'RecipeEditPage',
+  setup() {
+    const auth = firebase.auth();
+    const userRecipes = ref([]);
+    const loading = ref(true);
+
+    const fetchUserRecipes = async (userId) => {
+      try {
+        userRecipes.value = await getUserRecipes(userId);
+        loading.value = false;
+      } catch (error) {
+        console.error('Error fetching user recipes:', error);
+        // Handle error here (e.g., show error message)
+      }
+    };
+
+    onMounted(() => {
+      // Initialize onAuthStateChanged listener
+      // Unsubscribe from onAuthStateChanged when component is unmounted
+      return auth.onAuthStateChanged((user) => {
+        if (user) {
+          // User is signed in, fetch user recipes
+          fetchUserRecipes(user.uid);
+        } else {
+          // No user is signed in, reset recipes
+          userRecipes.value = [];
+          loading.value = false;
+        }
+      });
+    })
+
+    return {
+      userRecipes
+    }
+  }
 });
 </script>
 
