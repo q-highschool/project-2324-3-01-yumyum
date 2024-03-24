50d6cb806bdd6cd8daee1996975b4e4bb05c27d7
steven <steventevoortwis@gmail.com>
2024-03-24 17:17:20 +0100

    Updated some layouts

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -8,46 +8,73 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="drawer" class="bg-grey-3" style="text-align: center">
-        <q-btn rounded color="primary" no-caps icon-right="add" @click="addRecipe">New recipe&nbsp;</q-btn>
+    <q-drawer v-if="isUserPath" show-if-above v-model="drawer" class="bg-grey-3" style="text-align: center">
+      <q-btn rounded color="primary" no-caps icon-right="add" @click="addRecipe">New recipe&nbsp;</q-btn>
     </q-drawer>
 
-    <q-page-container>
-      <router-view/>
-    </q-page-container>
+    <div class="row">
+      <div class="col"/>
+
+      <div class="col-12" style="max-width: 1600px">
+        <q-page-container>
+          <router-view/>
+        </q-page-container>
+      </div>
+
+      <div class="col"/>
+    </div>
   </q-layout>
 </template>
 
 <script>
-import {defineComponent, ref} from 'vue';
-import {useRouter} from "vue-router";
+import { defineComponent, ref, watch } from 'vue';
+import { useRouter, useRoute } from "vue-router";
 import signOutUser from 'src/firebase/auth/firebase-signout'
-import {createRecipe} from "src/firebase/firestore/firestore-recipes";
+import { createRecipe } from "src/firebase/firestore/firestore-recipes";
 
 export default defineComponent({
   name: 'UserLayout',
   setup() {
-    const router = useRouter()
+    const router = useRouter();
+    const route = useRoute();
+
     const logout = () => {
       console.log("signout")
       signOutUser().then(() => {
-        router.push('/')
-      })
-    }
+        router.push('/');
+      });
+    };
 
     const addRecipe = () => {
       createRecipe().then((newRecipeId) => {
-        router.push('/user/e/' + newRecipeId)
-      })
-    }
+        router.push('/user/e/' + newRecipeId);
+      });
+    };
+
+    const drawer = ref(false);
+
+    // Watch route changes and update drawer visibility accordingly
+    watch(route, (to, from) => {
+      updateDrawerVisibility(to.path);
+    });
+
+    const isUserPath = ref(false);
+
+    const updateDrawerVisibility = (path) => {
+      isUserPath.value = path === '/user';
+    };
+
+    // Initialize drawer visibility based on current path
+    updateDrawerVisibility(route.path);
 
     return {
       logout,
-      drawer: ref(false),
-      addRecipe
-    }
+      drawer,
+      addRecipe,
+      isUserPath
+    };
   }
-})
+});
 </script>
 
 <style scoped>
