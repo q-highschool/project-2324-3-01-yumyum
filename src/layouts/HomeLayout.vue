39c71d2913170ad4cfa5ffc2463b1dcdee9da630
steven <steventevoortwis@gmail.com>
2024-03-24 15:35:19 +0100

    Fixed HomeLayout.vue

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,5 +1,12 @@
 <template>
   <q-layout>
+    <q-toolbar class="bg-amber">
+      <q-toolbar-title>YumYum</q-toolbar-title>
+
+      <q-btn color="primary" @click="this.$router.push('/auth/sign_up')">Sign up</q-btn>
+      <q-btn color="grey" @click="this.$router.push('/auth/sign_in')">Sign in</q-btn>
+    </q-toolbar>
+    
     <q-page-container>
       <router-view/>
     </q-page-container>
