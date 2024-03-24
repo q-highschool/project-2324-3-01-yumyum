358978d62322333ff852fadd75136989e6d440a9
steven <steventevoortwis@gmail.com>
2024-03-24 18:38:29 +0100

    Maybe also add the js functionality....

--- src/firebase/firestore/firestore-recipes.js
+++ src/firebase/firestore/firestore-recipes.js
@@ -28,3 +28,46 @@ export const createRecipe = () => {
     })
   })
 }
+
+
+export const getUserRecipes = (userId) => {
+  return new Promise((resolve, reject) => {
+    // Query recipes collection for recipes owned by the specified user
+    db.collection('recipes')
+      .where('owner', '==', userId)
+      .get()
+      .then((querySnapshot) => {
+        const userRecipes = [];
+        querySnapshot.forEach((doc) => {
+          // For each recipe found, push its data into the userRecipes array
+          userRecipes.push({id: doc.id, ...doc.data()});
+        });
+        resolve(userRecipes); // Resolve with the array of user's recipes
+      })
+      .catch((error) => {
+        reject(error); // Reject with the error if any
+      });
+  });
+};
+
+export const getSpecificRecipe = (recipeId) => {
+  return new Promise((resolve, reject) => {
+    db.collection('recipes').doc(recipeId).get().then((doc) => {
+      resolve(doc.data());
+    }).catch((error) => {
+      reject(error);
+    })
+  })
+}
+
+export const updateRecipe = (recipeId, newRecipeContent) => {
+  return new Promise((resolve, reject) => {
+    db.collection('recipes').doc(recipeId).update({
+      content: newRecipeContent
+    }).then(() => {
+      resolve()
+    }).catch((error) => {
+      reject(error);
+    })
+  })
+}
